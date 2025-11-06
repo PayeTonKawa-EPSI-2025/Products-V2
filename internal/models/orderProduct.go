92f11d906c736a3ff67c7b8c795baf1de5003641
Tanguy OZANO <tanguy.ozano@pit.fr>
2025-11-06 20:33:15 +0100

    fat(init): add association table

---
+++ internal/models/orderProduct.go
@@ -0,0 +1,11 @@
+package models
+
+import "github.com/PayeTonKawa-EPSI-2025/Common/models"
+
+type OrderProduct struct {
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	OrderID   uint           `json:"orderId"`
+	Order     Order          `gorm:"foreignKey:OrderID"`
+	ProductID uint           `json:"productId"`
+	Product   models.Product `gorm:"foreignKey:ProductID"`
+}
