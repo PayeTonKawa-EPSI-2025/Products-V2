44b5d2cfc3d8bcd7c618a3e8b64b79fb9cb0729a
Tanguy OZANO <tanguy.ozano@pit.fr>
2026-01-29 12:19:02 +0100

    fix(operation_test): fix wrong name

--- tests/integration/operation_test.go
+++ tests/integration/operation_test.go
@@ -4,20 +4,20 @@ import (
 	"context"
 	"testing"
 
-	"github.com/PayeTonKawa-EPSI-2025/Orders-V2/internal/operation"
+	"github.com/PayeTonKawa-EPSI-2025/Products-V2/internal/operation"
 )
 
-func TestIntegration_GetOrders(t *testing.T) {
+func TestIntegration_GetProducts(t *testing.T) {
 	db := ConnectDB(t)
-	ResetOrdersTable(t, db)
+	ResetProductsTable(t, db)
 	SeedDB(t, db)
 
-	resp, err := operation.GetOrders(context.Background(), db)
+	resp, err := operation.GetProducts(context.Background(), db)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 
-	if len(resp.Body.Orders) != 2 {
-		t.Fatalf("expected 2 customers, got %d", len(resp.Body.Orders))
+	if len(resp.Body.Products) != 2 {
+		t.Fatalf("expected 2 customers, got %d", len(resp.Body.Products))
 	}
 }
